4a96ba9f82c35ba14a19006c47944e621f651448
teng7715 <teng7715@gmail.com>
2024-05-31 10:10:57 +0800

    API writing and data processing completed

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+import mysql.connector
+
+
+db=mysql.connector.connect(
+	user="root",
+	password="123456787654321",
+	host="localhost",
+	database="Taipei_Day_Trip"
+)
+
+mycursor=db.cursor()
+
 app=FastAPI()
+
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
@@ -15,3 +28,142 @@ async def booking(request: Request):
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+
+# API
+@app.get("/api/attractions")
+async def get_attractions(request:Request,page:int=0,keyword:str=None):
+
+	try:
+
+		#先處理圖片URL的資料，再跑判斷式，因為無論有沒有keyword查詢，都需要用到
+		mycursor.execute("select attraction.id,url.image from url inner join attraction on attraction.id=url.attraction_id order by attraction.id")
+		image_datas=mycursor.fetchall() 			#裡面的資料是 [(1,'https:000848.jpg'),....]
+
+		image_grouped_data={}  						#裡面的資料，經過下面的迴圈後，會是{"1": ["https:...","https:..."], "2": ["https:...","https:..."]...}
+
+		for key,url in image_datas:
+			if key not in image_grouped_data:
+				image_grouped_data[key]=[]
+			image_grouped_data[key].append(url)
+
+		offset=page*12
+
+		if keyword==None or keyword=="":  #如果今天只需要針對頁碼做搜尋
+
+			mycursor.execute("select attraction.id,attraction.name,attraction.category,attraction.description,attraction.address,attraction.transport,station.mrt,attraction.lat,attraction.lng from attraction inner join station on attraction.station_id=station.id order by attraction.id limit %s,12",(offset,))
+			attraction_datas=mycursor.fetchall() 							#裡面的資料是[(1, '新北投溫泉區', '養生溫泉'...),(2, '大稻埕碼頭',...)...]
+			attraction_datas_list=[list(data) for data in attraction_datas] #attraction_datas裡面的資料需要轉換成list格式，我們才能在裡面新增資料，加上他們的景點照片
+
+
+			column_names=[desc[0] for desc in mycursor.description] 		#裡面的資料是['id', 'name', 'category', 'description', ...] 
+			column_names.append("images")
+
+
+			for attraction in attraction_datas_list:  						#用迴圈，在每個景點列表的最後方，加上他的景點照片資料
+				attraction.append(image_grouped_data[attraction[0]])
+			
+			cleaned_data=[dict(zip(column_names,attraction)) for attraction in attraction_datas_list]
+			
+			mycursor.execute("select count(id) from attraction")
+			attraction_count=mycursor.fetchone()[0] 						#印出來的會是Tuple-->(58,)這種形式，所以需要[0]
+	
+
+		else:  #如果今天想要搜尋keyword
+
+			keyword_param=f"%{keyword}%"
+			mycursor.execute("select attraction.id,attraction.name,attraction.category,attraction.description,attraction.address,attraction.transport,station.mrt,attraction.lat,attraction.lng from attraction inner join station on attraction.station_id=station.id where attraction.name like %s or station.mrt like %s order by attraction.id limit %s,12",(keyword_param,keyword_param,offset))
+			attraction_datas=mycursor.fetchall() 							
+			attraction_datas_list=[list(data) for data in attraction_datas]
+
+			column_names=[desc[0] for desc in mycursor.description] 		
+			column_names.append("images")
+
+			for attraction in attraction_datas_list:  						
+				attraction.append(image_grouped_data[attraction[0]])
+
+			cleaned_data=[dict(zip(column_names,attraction)) for attraction in attraction_datas_list]
+
+			mycursor.execute("select count(attraction.id) from attraction inner join station on attraction.station_id=station.id where attraction.name like %s or station.mrt like %s",(keyword_param,keyword_param))
+			attraction_count=mycursor.fetchone()[0]
+
+
+		page_num=attraction_count//12 #以總景點58為例，計算後會是4，表示page0.1.2.3頁時，nextPage都要有數字
+
+		if page < page_num:
+			next_page=page+1
+		else:
+			next_page=None 
+		
+
+		success_response={
+			"nextPage":next_page,
+			"data":cleaned_data
+		}
+
+		return JSONResponse(content=success_response,status_code=200)
+		
+
+	except Exception as e:
+		error_response={
+			"error":True,
+			"message":str(e)
+		}
+		return JSONResponse(content=error_response,status_code=500)
+
+
+@app.get("/api/attraction/{attractionId}")
+async def get_attraction(request:Request,attractionId:int):
+  
+	if attractionId <=0:
+			return JSONResponse(content={"error":True,"message":"景點編號查詢請輸入正整數！"},status_code=400)
+	try:
+		mycursor.execute("select attraction.id,attraction.name,attraction.category,attraction.description,attraction.address,attraction.transport,station.mrt,attraction.lat,attraction.lng from attraction inner join station on attraction.station_id=station.id where attraction.id=%s",(attractionId,))
+		attraction_datas_list=list(mycursor.fetchone())
+
+		column_names=[desc[0] for desc in mycursor.description]
+		column_names.append("images")
+
+		mycursor.execute("select url.image from url inner join attraction on attraction.id=url.attraction_id where attraction.id=%s",(attractionId,))
+		image_datas=mycursor.fetchall()
+		image_datas_list=[image[0] for image in image_datas]
+				
+		attraction_datas_list.append(image_datas_list)
+
+		cleaned_data=dict(zip(column_names,attraction_datas_list))
+
+		success_response={
+			"data":cleaned_data
+		}
+
+		return JSONResponse(content=success_response,status_code=200)
+
+	except Exception as e:
+		error_response={
+			"error":True,
+			"message":str(e)
+		}
+		return JSONResponse(content=error_response,status_code=500)
+
+
+@app.get("/api/mrts") 
+async def get_mrts(request:Request):
+	
+	try:
+		mycursor.execute("select station.mrt,count(*) as num_station_with_attraction from attraction inner join station on attraction.station_id=station.id group by station.mrt order by num_station_with_attraction DESC")
+		data=mycursor.fetchall()  #data裡的內容為[('新北投', 6), ('關渡', 4), ('劍潭', 4),....]等等透過Mysql已幫忙排序的資料
+
+		cleaned_data=[station[0] for station in data]
+
+		success_response={
+			"data":cleaned_data
+		}
+		return JSONResponse(content=success_response,status_code=200)
+
+	except Exception as e:
+		error_response={
+			"error":True,
+			"message":str(e)
+		}
+		return JSONResponse(content=error_response,status_code=500)
+	
---
+++ data/data-process.py
@@ -0,0 +1,73 @@
+import mysql.connector
+import json
+import re
+
+db=mysql.connector.connect(
+    user="root",
+    password="123456787654321",
+    host="localhost",
+    database="Taipei_Day_Trip"
+)
+
+mycursor=db.cursor()
+
+#####分隔線#####
+
+
+# 1/ 開啟檔案
+with open("taipei-attractions.json","r",encoding="utf-8") as file:
+    data=json.load(file)
+
+data=data["result"]["results"]
+
+
+# 2/ 將捷運資料整理後，寫入資料庫
+mrt_data=[]
+
+for x in range(len(data)):        # 針對58筆資料，透過迴圈，將每個景點的捷運站抓出來，並在不重複的前提下，組成一個新的，紀錄捷運站站名的列表
+    station=data[x]["MRT"]
+    if station not in mrt_data:
+        mrt_data.append(station)  # mrt_data裡，總共33筆資料，['新北投', '雙連', '士林', '劍潭'....]
+
+
+for station in mrt_data:
+    mycursor.execute("insert into station(mrt) values(%s)",(station,))
+
+
+# 2.5/ 將每一站的資料變成字典，好讓後面景點資料可以對照，取得id數值
+station_dict={} 
+
+for x in range(len(mrt_data)):
+    station_dict[mrt_data[x]]=x+1
+
+
+# 3/ 將景點資料整理後，寫入資料庫
+for x in range(len(data)): 
+    
+    name=data[x]["name"]
+    category=data[x]["CAT"]
+    description=data[x]["description"]
+
+    address=data[x]["address"] 
+    cleaned_address=address.replace(" ","")  # 因原始資料的地址中間有空格（臺北市  北投區中山路、光明路沿線），所以我們多一步來處理
+
+    transport=data[x]["direction"]
+    station_id=station_dict[data[x]["MRT"]]         # 這邊原本的值是是"新北投"，但透過字典station_dict比對後，取得到id數值
+    lat=data[x]["latitude"]
+    lng=data[x]["longitude"]
+
+    mycursor.execute("insert into attraction(name,category,description,address,transport,station_id,lat,lng) values(%s,%s,%s,%s,%s,%s,%s,%s)",(name,category,description,cleaned_address,transport,station_id,lat,lng))
+
+    # 4/ 將景點的照片資料整理後，寫入資料庫
+    attraction_id=x+1
+
+    images=data[x]["file"] 
+
+    rule=re.compile(r'(https?://\S+?\.(?:jpg|png))', re.I)
+    cleaned_images=rule.findall(images)  #leaned_images，會是比對後的所有網址，所形成得一個『列表』
+
+    for image in cleaned_images:
+        mycursor.execute("insert into url(attraction_id,image) values(%s,%s)",(attraction_id,image))
+
+
+db.commit() 
